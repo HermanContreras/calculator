cd6039dd8f3c1b3b474090379d0590e3171679f9
Herman <contreras.herman20@gmail.com>
2022-08-13 09:30:11 -0500

    Add the ability to press numbers and display said numbers

--- scripts.js
+++ scripts.js
@@ -1,4 +1,76 @@
-function addScreen(){
-    const screen = document.querySelector('.screen');
+let bottomDisplay = '',
+    topDisplay    = '',
+    and1, and2, operator;
+
+function operate(a, b, operator){
+    switch(operator){
+        case '/':
+            return divide(a, b);
+        case '*':
+            return multiply(a, b);
+        case '+':
+            return add(a,b);
+        case '-':
+            return subtract(a,b);
+    }
 
 }
+
+function addTopDisplay(display){
+    const topScreen  = document.querySelector('.topDisplay');
+    topScreen.textContent = topDisplay;
+}
+
+function addBottomScreen(){
+    
+    const bottomScreen  = document.querySelector('.bottomDisplay');
+
+    if(bottomScreen.textContent) bottomScreen.textContent = '';
+    bottomScreen.textContent = bottomDisplay;
+}
+
+function divide(a, b){
+    return (a/b).toFixed(2);
+}
+
+function multiply(a, b){
+    return a * b;
+}
+
+function subtract(a, b){
+    return a-b;
+}
+
+function add(a, b){
+    return a + b;
+}
+
+function getClickHelper(e){
+    let input = e.target.id;
+    
+    if(input == '/' || input == '*' || input =='+' || input == '-'){
+        topDisplay += (bottomDisplay + input),
+        and1     = +bottomDisplay,
+        operator = input,
+        bottomDisplay = '';
+        console.log(operator);
+        addTopDisplay(topDisplay);
+    }
+    else if(input == 'equals'){
+        console.log(input);
+    }
+    else  bottomDisplay += input;
+        
+    addBottomScreen(input);
+}
+
+function getClick(){
+    const buttons = Array.from(document.querySelectorAll('.btn'));
+
+    buttons.forEach(btn =>
+        btn.addEventListener ('click',e =>
+            getClickHelper(e)
+        ));
+}   
+
+getClick();
